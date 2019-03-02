80cee1f7a509577e36e2325379b24d3153e56f48
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-03-02 23:02:41 +0700

    implement default XLPagerTabStrip with 2 child view controller

---
+++ PhoneGuide/FavoriteListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteListViewController.swift
+//  PhoneGuide
+//
+//  Created by Thanaphat Suwannikornkul on 02/03/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class FavoriteListViewController: UIViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    // MARK: - IndicatorInfoProvider
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "FavoriteList")
+    }
+}
--- PhoneGuide/ViewController.swift
+++ PhoneGuide/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class ViewController: UIViewController {
+class ViewController: ButtonBarPagerTabStripViewController {
 
+    let purpleInspireColor = UIColor(red:0.13, green:0.03, blue:0.25, alpha:1.0)
+    
     override func viewDidLoad() {
+        // change selected bar color
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = purpleInspireColor
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = self?.purpleInspireColor
+        }
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let fullListVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "fullListVC")
+        let favoriteListVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "favoriteListVC")
+        return [fullListVC, favoriteListVC]
+    }
 }
 
