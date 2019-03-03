7864ade37991c85e4dbf85273ffd26356767f30a
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-03-03 14:26:15 +0700

    init MobileTableViewCell

---
+++ PhoneGuide/Views/MobileTableViewCell/MobileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MobileTableViewCell.swift
+//  PhoneGuide
+//
+//  Created by Thanaphat Suwannikornkul on 03/03/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+class MobileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
